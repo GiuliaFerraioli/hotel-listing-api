63c65f246d692e19c1cc2c424642b63eb3ef171c
GiuliaFe <ferraioligiulia@gmail.com>
2024-03-08 18:08:51 +0000

    see description
    in previous commits:
    CORS configuration
    Serilog and Seq configuration
    Entity framework setup
    in this commit:
    Perform migration
    Seed data in tables

--- Data/HotelListingDBContext.cs
+++ Data/HotelListingDBContext.cs
@@ -5,8 +5,8 @@ namespace HotelListing.API.Data
     public class HotelListingDBContext : DbContext
     {
         public HotelListingDBContext(DbContextOptions options) : base(options)
-        { 
-           
+        {
+
         }
 
         public DbSet<Hotel> Hotels { get; set; }
@@ -15,6 +15,55 @@ namespace HotelListing.API.Data
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasData(
+                new Country
+                {
+                    Id = 1,
+                    Name = "Jamaica",
+                    ShortName = "JM"
+                },
+                new Country
+                {
+                    Id = 2,
+                    Name = "Bahamas",
+                    ShortName = "BS"
+                },
+                new Country
+                {
+                    Id = 3,
+                    Name = "Cayman Island",
+                    ShortName = "CI"
+                }
+                );
+
+            modelBuilder.Entity<Hotel>().HasData(
+                new Hotel
+                {
+                    Id = 1,
+                    Name = "Sandals Resort and Spa",
+                    Adress = "Negril",
+                    CountryId = 1,
+                    Rating = 4.5
+
+                },
+                new Hotel
+                {
+                    Id = 2,
+                    Name = "Comfort Suites",
+                    Adress = "George Town",
+                    CountryId = 3,
+                    Rating = 4.3
+                },
+                new Hotel
+                {
+                    Id = 3,
+                    Name = "Grand Palldium",
+                    Adress = "Nssua",
+                    CountryId = 1,
+                    Rating = 4
+                }
+                );
+
         }
     }
 }
