aaef5cd1760ad5aa5ecc3065df832c6ff3d6be49
GiuliaFe <ferraioligiulia@gmail.com>
2024-03-25 18:01:33 +0000

    add scaffolding controller for hotels

--- Models/Hotels/HotelDto.cs
+++ Models/Hotels/HotelDto.cs
@@ -2,14 +2,8 @@
 
 namespace HotelListing.API.Models.Hotels
 {
-    public class HotelDto
+    public class HotelDto : BaseHotelDto
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Adress { get; set; }
-        public double Rating { get; set; }
-
-        [ForeignKey(nameof(CountryId))]
-        public int CountryId { get; set; }
     }
 }
