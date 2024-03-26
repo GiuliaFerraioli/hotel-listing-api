2419e16e42ef2fb8f893230177ad701adbbfe800
GiuliaFe <ferraioligiulia@gmail.com>
2024-03-26 16:04:42 +0000

    add default user roles

---
+++ Data/Configurations/HotelConfiguration.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System.Reflection.Emit;
+
+namespace HotelListing.API.Data.Configurations
+{
+    public class HotelConfiguration : IEntityTypeConfiguration<Hotel>
+    {
+        public void Configure(EntityTypeBuilder<Hotel> builder)
+        {
+            builder.HasData(
+                new Hotel
+                {
+                    Id = 1,
+                    Name = "Sandals Resort and Spa",
+                    Adress = "Negril",
+                    CountryId = 1,
+                    Rating = 4.5
+
+                },
+                new Hotel
+                {
+                    Id = 2,
+                    Name = "Comfort Suites",
+                    Adress = "George Town",
+                    CountryId = 3,
+                    Rating = 4.3
+                },
+                new Hotel
+                {
+                    Id = 3,
+                    Name = "Grand Palldium",
+                    Adress = "Nssua",
+                    CountryId = 1,
+                    Rating = 4
+                }
+                );
+        }
+    }
+}
---
+++ Migrations/20240326160226_AddedDefaultRoles.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace HotelListing.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedDefaultRoles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "0b26891a-cbec-419c-8bf0-c1c78b13e8db", null, "User", "USER" },
+                    { "5808b903-9861-4946-8e8d-d1124c1d1691", null, "Administrator", "ADMINISTRATOR" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "0b26891a-cbec-419c-8bf0-c1c78b13e8db");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "5808b903-9861-4946-8e8d-d1124c1d1691");
+        }
+    }
+}
